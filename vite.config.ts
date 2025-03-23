7c95fc6ab97fc0afd6abce3a16ef0c23801ac825
2562374500 <2562374500@qq.com>
2025-03-24 02:09:54 +0800

    简化构建配置，修复 Vue 运行时错误

--- vite.config.ts
+++ vite.config.ts
@@ -16,21 +16,12 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: undefined,
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          let extType = info[info.length - 1]
-          if (/\.(png|jpe?g|gif|svg|webp|ico)(\?.*)?$/.test(assetInfo.name)) {
-            extType = 'img'
-          } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/.test(assetInfo.name)) {
-            extType = 'fonts'
-          } else if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/.test(assetInfo.name)) {
-            extType = 'media'
-          }
-          return `assets/${extType}/[name]-[hash][extname]`
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router'],
         },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
       },
     },
   },
